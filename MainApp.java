d4bf7798a5af20b8baf71b3c0fe92aef825f14ec
Eric Chauvin <ericsourcecode@gmail.com>
2018-11-21 11:55:03 -0700

    Update MainApp.java

--- MainApp.java
+++ MainApp.java
@@ -1,6 +1,23 @@
 // Copyright Eric Chauvin 2018.
-// My blog is at:
-// ericsourcecode.blogspot.com
+
+
+// Layout managers
+// https://docs.oracle.com/javase/tutorial/uiswing/layout/index.html
+
+
+
+
+// Text Area:
+// https://docs.oracle.com/javase/tutorial/uiswing/components/textarea.html
+
+
+
+
+// https://docs.oracle.com/javase/tutorial/uiswing/components/tabbedpane.html
+
+
+// Swing Tutorial:
+// https://docs.oracle.com/javase/tutorial/uiswing/index.html
 
 
 // Menus:
@@ -10,22 +27,11 @@
 // Javac:
 // https://docs.oracle.com/javase/7/docs/technotes/tools/windows/javac.html
 
-// Swing Tutorial:
-// https://docs.oracle.com/javase/tutorial/uiswing/index.html
-
 // Frames tutorial and other tutorials:
 // https://docs.oracle.com/javase/tutorial/uiswing/components/frame.html
 
 
-// Getting Started with Swing
 
-// Using Swing Components
-//    tells you how to use each of the Swing components
-
-
-// "If you would like to incorporate JavaFX into your
-// Swing application, please see Integrating JavaFX
-// into Swing Applications."
 
 
 ///////////////////////////////
@@ -47,102 +53,17 @@ import java.awt.event.ActionEvent;
 import javax.swing.KeyStroke;
 import java.awt.Font;
 import java.awt.Color;
+import javax.swing.JTabbedPane;
+import javax.swing.JTextArea;
+import javax.swing.JScrollPane;
 
 
 
 public class MainApp implements Runnable
   {
-  public static final String VersionDate = "4/18/2018";
-
-  // https://docs.oracle.com/javase/7/docs/api/javax/swing/JMenuBar.html
-  private JMenuBar menuBar;
-  private JMenu menu;
-  private JMenu submenu;
-  private JMenuItem menuItem;
-
-
-
-  // For the Runnable interface:
-  @Override
-  public void run()
-    {
-    // Create the window
-    JFrame MFrame = new JFrame( "Code Editor" );
-
-
-    MFrame.setDefaultCloseOperation( WindowConstants.EXIT_ON_CLOSE );
-    MFrame.setLayout( new FlowLayout() );
-    MFrame.add( new JLabel( "Code Editor" ));
-    MFrame.add( new JButton( "For App" ) );
-
-    // Container c = JFrame.getContentPane();
-    // c.setBackground(Color.red);
-    MFrame.getContentPane().setBackground( Color.red );
-    MFrame.getContentPane().setForeground( Color.white );
-
-
-    menuBar = new JMenuBar();
-    menuBar.setBackground( Color.black );
-
-    menu = new JMenu( "A Menu" );
-    menu.setMnemonic( KeyEvent.VK_A );
-    // Font  f1  = new Font( Font.SERIF, Font.PLAIN, 50 );
-    Font  f1  = new Font( Font.MONOSPACED, Font.PLAIN, 50 );
-
-    menu.setFont( f1 );
-    menu.setForeground( Color.white );
-
-
-    menuBar.add( menu );
-
-    menuItem = new JMenuItem( "A text-only menu item",
-                         KeyEvent.VK_T);
-    menuItem.setAccelerator( KeyStroke.getKeyStroke(
-        KeyEvent.VK_1, ActionEvent.ALT_MASK ));
-    menu.add( menuItem );
-
-    menuItem = new JMenuItem( "Second one." );
-    menuItem.setMnemonic( KeyEvent.VK_B );
-    menu.add( menuItem );
-
-    // menu.addSeparator();
-
-    menu.addSeparator();
-    submenu = new JMenu( "A submenu" );
-    submenu.setMnemonic(KeyEvent.VK_S);
-
-    menuItem = new JMenuItem("An item in the submenu");
-    menuItem.setAccelerator(KeyStroke.getKeyStroke(
-        KeyEvent.VK_2, ActionEvent.ALT_MASK));
-    submenu.add(menuItem);
-
-    menuItem = new JMenuItem("Another item");
-    submenu.add(menuItem);
-    menu.add(submenu);
-
-    menu = new JMenu("Another Menu");
-    menu.setMnemonic(KeyEvent.VK_N);
-    menuBar.add(menu);
-
-
-    MFrame.setJMenuBar( menuBar );
-
-
-
-
-    // MFrame.setSize()
-
-    // Set the frame icon to an image loaded from a
-    // file.
-    // MFrame.setIconImage(new ImageIcon(imgURL).getImage());
-
-    MFrame.pack();
-
-    // Center it.
-    MFrame.setLocationRelativeTo( null );
-
-    MFrame.setVisible( true );
-    }
+  public static final String VersionDate = "11/15/2018";
+  public JTabbedPane TabbedPane;
+  public JFrame MainFrame;
 
 
 
@@ -158,5 +79,126 @@ public class MainApp implements Runnable
 
 
 
+  // For the Runnable interface:
+  @Override
+  public void run()
+    {
+    SetupMainFrame();
+    SetupMenus();
+    AddTextPane();
+    }
+
+
+
+  public void AddTextPane()
+    {
+    // tabbedPane.setMnemonicAt(0, KeyEvent.VK_1);
+
+    JTextArea TextArea1 = new JTextArea();
+    Font  F1  = new Font( Font.MONOSPACED, Font.PLAIN, 50 );
+    TextArea1.setFont( F1 );
+    TextArea1.setLineWrap( true );
+    TextArea1.setWrapStyleWord( true );
+    // TextArea1.setBackground( Color.black );
+    // TextArea1.setForeground( Color.white );
+
+    JScrollPane ScrollPane1 = new JScrollPane( TextArea1 );
+
+    ScrollPane1.setVerticalScrollBarPolicy(
+             JScrollPane.VERTICAL_SCROLLBAR_ALWAYS );
+
+    TabbedPane.addTab( "Text 1", null, ScrollPane1,
+                      "Tool tip text for 1" );
+    }
+
+
+
+
+  public void SetupMainFrame()
+    {
+    MainFrame = new JFrame( "Code Editor" );
+    MainFrame.setDefaultCloseOperation(
+                     WindowConstants.EXIT_ON_CLOSE );
+    MainFrame.setLayout( new FlowLayout() );
+
+    // MainFrame.add( new JLabel( "Code Editor" ));
+    // MainFrame.add( new JButton( "For App" ) );
+
+    TabbedPane = new JTabbedPane();
+    MainFrame.add( TabbedPane );
+
+
+    // Container c = JFrame.getContentPane();
+    // c.setBackground(Color.red);
+    MainFrame.getContentPane().setBackground( Color.red );
+    MainFrame.getContentPane().setForeground( Color.white );
+
+    // MainFrame.setSize()
+
+    // Set the frame icon to an image loaded from a
+    // file.
+    // MFrame.setIconImage(new ImageIcon(imgURL).getImage());
+
+    // MainFrame.pack();
+
+    // Center it.
+    // MainFrame.setLocationRelativeTo( null );
+
+    MainFrame.setExtendedState( JFrame.MAXIMIZED_BOTH );
+    MainFrame.setVisible( true );
+    }
+
+
+
+  private void SetupMenus()
+    {
+    JMenuBar menuBar = new JMenuBar();
+    menuBar.setBackground( Color.black );
+
+    JMenu menu = new JMenu( "A Menu" );
+    menu.setMnemonic( KeyEvent.VK_A );
+    // Font  f1  = new Font( Font.SERIF, Font.PLAIN, 50 );
+    Font  f1  = new Font( Font.MONOSPACED, Font.PLAIN, 50 );
+
+    menu.setFont( f1 );
+    menu.setForeground( Color.white );
+
+    menuBar.add( menu );
+
+    JMenuItem menuItem = new JMenuItem( "A text-only menu item",
+                         KeyEvent.VK_T);
+    menuItem.setAccelerator( KeyStroke.getKeyStroke(
+        KeyEvent.VK_1, ActionEvent.ALT_MASK ));
+    menu.add( menuItem );
+
+    menuItem = new JMenuItem( "Second one." );
+    menuItem.setMnemonic( KeyEvent.VK_B );
+    menu.add( menuItem );
+
+    menu.addSeparator();
+
+    JMenu submenu = new JMenu( "A submenu" );
+    submenu.setMnemonic(KeyEvent.VK_S);
+
+    menuItem = new JMenuItem("An item in the submenu");
+    menuItem.setAccelerator(KeyStroke.getKeyStroke(
+        KeyEvent.VK_2, ActionEvent.ALT_MASK));
+    submenu.add(menuItem);
+
+    menuItem = new JMenuItem("Another item");
+    submenu.add(menuItem);
+    menu.add(submenu);
+
+    menu = new JMenu("Another Menu");
+    menu.setMnemonic(KeyEvent.VK_N);
+    menuBar.add(menu);
+
+    // System.exit( 0 );
+
+    MainFrame.setJMenuBar( menuBar );
+    }
+
+
+
+
   }
-  
